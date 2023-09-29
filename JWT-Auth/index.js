2511e10f2cbb1ba63e8eb88d3fb4f274805bb9a7
Balascan Gabriel-Danut <47115084+BalascanGabriel@users.noreply.github.com>
2023-09-29 13:01:09 +0300

    .

--- JWT-Auth/index.js
+++ JWT-Auth/index.js
@@ -1,41 +1,63 @@
 require('dotenv').config()
 const express = require('express')
-var app = express()
-
+const app = express()
 const jwt = require('jsonwebtoken')
 
-//Permite aplicatiei sa foloseasca jsoane din bidy
 app.use(express.json())
+const port = process.env.PORT
 
 const posts = [
     {
-        username: "Gabe",
-        post: "Caut mixer dj Pioneer"
+        username: 'Kyle',
+        title: 'Post 1'
     },
     {
-        username: "Matei",
-        post: "Vand consola dj Traktor"
+        username: 'Brian',
+        title: 'Post 2',
+    },
+    {
+        username: 'Kyle',
+        title: 'Post 43'
     }
 ]
 
 app.get('/', (req, res) => {
-    res.send("Salut vere")
+    res.send('<h1>Welcome to home page</h1>')
 })
 
-app.get('/posts', (req, res) => {
-    res.json(posts)
+app.get('/posts', authenticateToken, (req, res) => {
+    console.log('Posts:', posts); // Debugging line
+    res.json(posts.filter(post => post.username === req.user.name))
 
 })
 
 app.post('/login', (req, res) => {
 
     const username = req.body.username
-    const user = {name : username}
+    const user = { name: username }
 
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({accessToken : accessToken})
+    res.json({ accessToken: accessToken })
 })
 
-app.listen(3000,()=>{
-    console.log('Server started.........')
+//Auth middleware
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null)
+        return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user
+        console.log('User:', req.user); // Add this line for debugging
+        next()
+    })
+
+}
+
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}.....`)
 })
